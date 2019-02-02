07b6e51eca68e6533a4a1879e326034e547607a1
IrodaMaksudova <iroda.maksudova@gmail.com>
2019-02-01 21:11:34 -0500

    Reorganize annotations and close driver

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -27,6 +28,12 @@ public class LoginTests {
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test");
         Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+
+    }
+@AfterMethod
+    public void cleanUp(){
+        driver.close();
+}
 
     }
 }

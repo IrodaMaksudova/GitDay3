c2bf9515171c3432552de8c7d6156cca5b412d03
IrodaMaksudova <iroda.maksudova@gmail.com>
2019-02-08 20:35:26 -0500

    add hashmap

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -12,11 +12,15 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTests {
 
     WebDriver driver;
+    Map<String,String> myMap = new HashMap<String, String>();
+
 //testing
     @BeforeMethod
     public void setUp() {
